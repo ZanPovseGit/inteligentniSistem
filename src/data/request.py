d47672aa7ef915aea0c709a230c861b2b9611172
zan <zan.povse@student.um.si>
2024-03-21 23:03:04 +0100

    spremembe nalaganja

--- src/data/request.py
+++ src/data/request.py
@@ -28,18 +28,27 @@ def save_json_to_file_per_name(df, df2, folder_path):
     for name in unique_names:
         df_filtered = df[df['name'] == name]
         df_filtered = pd.concat([df_filtered]*len(df2), ignore_index=True)
-        df_filtered = pd.concat([df_filtered, df2.reset_index(drop=True)], axis=1)        
+        df_filtered = pd.concat([df_filtered, df2.reset_index(drop=True)], axis=1)
         data_to_save = df_filtered.to_dict(orient='records')
         file_path = os.path.join(folder_path, f'{name}.json')
-        with open(file_path, 'a') as json_file:
-            json.dump(data_to_save, json_file, indent=2)
+        if os.path.exists(file_path):
+            with open(file_path, 'r') as json_file:
+                existing_data = json.load(json_file)
+        else:
+            existing_data = []
+        
+        existing_data.extend(data_to_save)
+        
+        with open(file_path, 'w') as json_file:
+            json.dump(existing_data, json_file, indent=2)
+        
         print(f"JSON file saved successfully for {name} at {file_path}")
 
 
 def main():
     api_url = 'https://api.jcdecaux.com/vls/v1/stations?contract=maribor&apiKey=5e150537116dbc1786ce5bec6975a8603286526b'
     api_url2 = 'https://api.open-meteo.com/v1/forecast?latitude=46.5547&longitude=15.6467&current=temperature_2m,rain,weather_code&timezone=Europe%2FBerlin&forecast_days=1'
-    folder_path = r'C:\Users\Uporabnik\Desktop\notebooks\inteligVaje\data\raw'
+    folder_path = r'C:\Users\Uporabnik\Desktop\notebooks\inteligVaje\data\processed'
     json_data = fetch_json_from_api(api_url)
     weather_data = fetch_json_from_api(api_url2)
 
--- src/data/requestGit.py
+++ src/data/requestGit.py
@@ -28,14 +28,20 @@ def save_json_to_file_per_name(df, df2, folder_path):
     for name in unique_names:
         df_filtered = df[df['name'] == name]
         df_filtered = pd.concat([df_filtered]*len(df2), ignore_index=True)
-        df_filtered = pd.concat([df_filtered, df2.reset_index(drop=True)], axis=1)        
+        df_filtered = pd.concat([df_filtered, df2.reset_index(drop=True)], axis=1)
         data_to_save = df_filtered.to_dict(orient='records')
         file_path = os.path.join(folder_path, f'{name}.json')
-        with open(file_path, 'a+') as json_file:
-            json_file.seek(0, os.SEEK_END)
-            if json_file.tell() != 0:
-                json_file.write('\n')
-            json.dump(data_to_save, json_file, indent=2)
+        if os.path.exists(file_path):
+            with open(file_path, 'r') as json_file:
+                existing_data = json.load(json_file)
+        else:
+            existing_data = []
+        
+        existing_data.extend(data_to_save)
+        
+        with open(file_path, 'w') as json_file:
+            json.dump(existing_data, json_file, indent=2)
+        
         print(f"JSON file saved successfully for {name} at {file_path}")
 
 
