e75b92c5782e39edfccd9d2db76e463a7d2464f8
zan <zan.povse@student.um.si>
2024-04-22 12:37:08 +0200

    DVCpush53

--- src/data/expect.py
+++ src/data/expect.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 
 
-df = pd.read_csv("data/tempdata/DVORANA TABOR.json")
+df = pd.read_csv("data/tempdata/porcessed/DVORANA TABOR.json")
 
 expectation_suite = gx.expectations("NewSuite")
 validation_results = df.validate(expectation_suite)
