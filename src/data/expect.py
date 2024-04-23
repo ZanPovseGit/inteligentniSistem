e5c6cf77a9dc36d9401ebb20ebd957b7558d04b4
zan <zan.povse@student.um.si>
2024-04-23 12:13:31 +0200

    Napaka

--- src/data/expect.py
+++ src/data/expect.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 
 
-df = pd.read_csv("data/tempdata/processed/DVORANA TABOR.json")
+df = pd.read_json("data/tempdata/processed/DVORANA TABOR.json")
 
 expectation_suite = gx.expectations("NewSuite")
 validation_results = df.validate(expectation_suite)
