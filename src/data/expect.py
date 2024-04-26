a0377927e69aaf196b31f6a06387dc38fbda4390
zan <zan.povse@student.um.si>
2024-04-26 12:39:06 +0200

    Expectations3

--- src/data/expect.py
+++ src/data/expect.py
@@ -1,12 +1,16 @@
 from collections.abc import Iterable
 import great_expectations as gx
 import pandas as pd
+import glob
 
 
 
+file_list = glob.glob("data/tempdata/processed/*TABOR.json")
 
-df = pd.read_json("data/tempdata/processed/DVORANA_TABOR.json")
-
-expectation_suite = gx.expectations("NewSuite")
-validation_results = df.validate(expectation_suite)
-print(validation_results)
+if file_list:
+    df = pd.read_json(file_list[0])
+    expectation_suite = gx.expectations("NewSuite")
+    validation_results = df.validate(expectation_suite)
+    print(validation_results)
+else:
+    print("No files matching the pattern found.")
