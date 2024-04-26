683cb5d55db7e619d917367ea23e527fe4cba2da
zan <zan.povse@student.um.si>
2024-04-26 16:42:55 +0200

    Expectations9

--- src/data/expect.py
+++ src/data/expect.py
@@ -3,23 +3,28 @@ import pandas as pd
 import os
 import glob
 
-# Assuming you have already created a DataContext and an ExpectationSuite
-context = gx.get_context()
+context = DataContext('gx')
+
 expectation_suite = context.get_expectation_suite("NewSuite")
+
 
 # Load your data
 file_list = glob.glob("data/tempdata/processed/*TABOR.json")
 if file_list:
-    df = pd.read_json(file_list[0])
-
-    # Validate your data using the ExpectationSuite
-    validation_result = context.validate_dataframe(
-        dataframe=df,
-        expectation_suite=expectation_suite,
-        batch_kwargs={"batch_identifiers": {"file_path": file_list[0]}}
+    json_data = pd.read_json(file_list[0])
+    validation_results = context.run_validation_operator(
+    "action_list_operator",  # You might need to adjust this based on your setup
+    assets_to_validate=[json_data],  # Pass the JSON data to be validated
+    run_id="id1"  # Provide a unique run_id for tracking
     )
 
-    # Print the validation results
-    print(validation_result)
+    # Check validation results
+    if validation_results["success"]:
+        print("Data meets expectation!")
+    else:
+        print("Data does not meet expectation. Validation errors:")
+        for result in validation_results["validation_result"]:
+            print(result)
+
 else:
     print("No files matching the pattern found.")
