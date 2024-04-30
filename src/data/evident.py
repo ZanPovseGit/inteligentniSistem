daad3020d3691a79af14175a62c8be8276ec8b69
zan <zan.povse@student.um.si>
2024-05-01 00:18:59 +0200

    EvidentlyTEST3

--- src/data/evident.py
+++ src/data/evident.py
@@ -7,17 +7,10 @@ import os
 from evidently import ColumnMapping
 
 from evidently.report import Report
-from evidently.metrics.base_metric import generate_column_metrics
-from evidently.metric_preset import DataDriftPreset, TargetDriftPreset
+from evidently.metric_preset import DataDriftPreset
 from evidently.metrics import *
 
-from evidently.test_suite import TestSuite
-from evidently.tests.base_test import generate_column_tests
-from evidently.test_preset import DataStabilityTestPreset, NoTargetPerformanceTestPreset
-from evidently.tests import *
-
-
-df = pd.read_json('data/tempdata/processed/DVORANA_TABOR.json')
+df = pd.read_json('data/tempdata/processed/DVORANA_TABOR.json'):
 
 df.drop(columns=['position'], inplace=True)
 df.drop(columns=['banking'], inplace=True)
@@ -31,9 +24,8 @@ midpoint = len(df) // 2
 reference = df.iloc[:midpoint]
 current = df.iloc[midpoint:]
 
-
 report = Report(metrics=[
-    DataDriftPreset(), 
+    DataDriftPreset(),
 ])
 
 report.run(reference_data=reference, current_data=current)
