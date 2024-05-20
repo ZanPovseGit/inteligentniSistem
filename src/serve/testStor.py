f10b50d12d3aab5b4474fe2081b66370d60a3dbb
zan <zan.povse@student.um.si>
2024-05-20 12:19:46 +0200

    Zadnja4

--- src/serve/testStor.py
+++ src/serve/testStor.py
@@ -1,28 +1,24 @@
 import pytest
 import json
 import requests
-from serviceNormal import appImp
-from serviceNormal import DateTimeTransformer
+from serviceDruga import app
 import pandas as pd
 
 @pytest.fixture
 def client():
-    with appImp.test_client() as client:
+    with app.test_client() as client:
         yield client
 
 def test_predict_endpoint(client):
     try:
-        # Assuming DateTimeTransformer is imported correctly and available in serviceNormal module
-        transformer = DateTimeTransformer()
-
-        url = '/predictLas'
+        url = '/predict'
         test_data = {
             "target": 1.0,
             "rain": 0.2,
             "time": "2024-05-15T14:30:00"
         }
 
-        response = client.post(url, json=test_data)
+        response = client.get(url)
 
         assert response.status_code == 200
 
